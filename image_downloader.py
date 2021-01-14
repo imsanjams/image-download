88ad739c25ac045dbafa44136a6204aa1c5fd764
Sanjam Singh <47633733+ssanjam20@users.noreply.github.com>
2021-01-14 14:57:43 +0530

    Add files via upload

---
+++ image_downloader.py
@@ -0,0 +1,28 @@
+import argparse
+import pd
+import os
+import tqdm 
+import urllib.request
+import np
+import sys
+
+parser = argparse.ArgumentParser(description='r/Fakeddit image downloader')
+
+parser.add_argument('type', type=str, help='train, validate, or test')
+
+args = parser.parse_args()
+
+df = pd.read_csv(args.type, sep="\t")
+df = df.replace(np.nan, '', regex=True)
+df.fillna('', inplace=True)
+
+pbar = tqdm(total=len(df))
+
+if not os.path.exists("images"):
+  os.makedirs("images")
+for index, row in df.iterrows():
+  if row["hasImage"] == True and row["image_url"] != "" and row["image_url"] != "nan":
+    image_url = row["image_url"]
+    urllib.request.urlretrieve(image_url, "images/" + row["id"] + ".jpg")
+  pbar.update(1)
+print("done")
